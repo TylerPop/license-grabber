355006171eb88be9529359d3157a6bcd25051ec0
Tyler <tapoplawski59@gmail.com>
2023-05-02 11:39:14 -0700

    Update index.js

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import packageJsonData from "../test-project/package.json" assert { type: "json" };
+import path from "path";
+import fs from "fs";
+
+const dependencies = Object.keys(packageJsonData.dependencies);
+const devDependencies = Object.keys(packageJsonData.devDependencies);
+
+const packageNames = dependencies.concat(devDependencies);
+packageNames.sort();
+
+const PROJECT_DIRECTORY = "./test-project";
+const NODE_MODULES = path.resolve(PROJECT_DIRECTORY, "node_modules");
+
+function getLicenseFiles(packageName) {
+  const licenseRegex = /(LICENSE|LICENCE|COPYING|COPYRIGHT)/gi;
+  const packagePath = path.join(NODE_MODULES, packageName);
+  const files = fs.readdirSync(packagePath);
+
+  const licenseFiles = files.filter((filename) => licenseRegex.test(filename));
+
+  return licenseFiles;
+}
+
+console.log(getLicenseFiles("rimraf"));

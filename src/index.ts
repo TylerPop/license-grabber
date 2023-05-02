662bde8c2acb29c9c1f678f37071064898416b5c
Tyler <tapoplawski59@gmail.com>
2023-05-02 14:09:14 -0700

    Add prettier formatting

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import packageJsonData from "../test-project/package.json";
-import path from "path";
-import fs from "fs";
+import packageJsonData from '../test-project/package.json';
+import path from 'path';
+import fs from 'fs';
 
 const dependencies = Object.keys(packageJsonData.dependencies);
 const devDependencies = Object.keys(packageJsonData.devDependencies);
@@ -8,8 +8,8 @@ const devDependencies = Object.keys(packageJsonData.devDependencies);
 const packageNames = dependencies.concat(devDependencies);
 packageNames.sort();
 
-const PROJECT_DIRECTORY = "./test-project";
-const NODE_MODULES = path.resolve(PROJECT_DIRECTORY, "node_modules");
+const PROJECT_DIRECTORY = './test-project';
+const NODE_MODULES = path.resolve(PROJECT_DIRECTORY, 'node_modules');
 
 function getLicenseFiles(packageName: string) {
   const licenseRegex = /(LICENSE|LICENCE|COPYING|COPYRIGHT)/gi;
@@ -21,4 +21,4 @@ function getLicenseFiles(packageName: string) {
   return licenseFiles;
 }
 
-console.log(getLicenseFiles("rimraf"));
+console.log(getLicenseFiles('rimraf'));

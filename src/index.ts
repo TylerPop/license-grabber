a89bedc889e47d3e70ff51285a196cdef6cac597
Tyler <tapoplawski59@gmail.com>
2023-05-02 17:50:43 -0700

    Add error handling for invalid dir or missing package.json

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,6 @@ import fs from 'fs';
 import * as LicenseUtils from './LicenseUtils';
 import { PackageData, PackageJson } from './PackageData';
 
-const PROJECT_DIRECTORY = '.';
-const NODE_MODULES_PATH = path.resolve(PROJECT_DIRECTORY, 'node_modules');
-const USE_NODE_MODULES = fs.existsSync(NODE_MODULES_PATH);
-
-const PACKAGE_JSON_BUFFER = fs.readFileSync(path.resolve(PROJECT_DIRECTORY, 'package.json'));
-const PACKAGE_JSON: PackageJson = JSON.parse(PACKAGE_JSON_BUFFER.toString());
-
 function getDependencies(packageJson: PackageJson, includeDevDependencies = true): PackageData[] {
   let dependencies: PackageData[] = [];
   let devDependencies: PackageData[] = [];
@@ -29,15 +22,34 @@ function getDependencies(packageJson: PackageJson, includeDevDependencies = true
   return dependencies.concat(devDependencies);
 }
 
-const packages = getDependencies(PACKAGE_JSON);
-
 function getAllPackagesData(basePath: string): PackageData[] {
+  const packages = getDependencies(PACKAGE_JSON);
   return packages.map((packageData) => {
     const packagePath = path.join(basePath, packageData.name);
     packageData.license = LicenseUtils.getLicenseInfo(packagePath);
     return packageData;
   });
 }
+
+const PROJECT_DIRECTORY = '.';
+let PACKAGE_JSON_BUFFER: Buffer;
+let PACKAGE_JSON: PackageJson;
+
+// Stop process if selected project directory does not exist
+if (!fs.existsSync(PROJECT_DIRECTORY)) {
+  console.error(`Error: ${PROJECT_DIRECTORY} does not exist.`);
+  process.exit(9); // Invalid Argument exit code
+}
+
+try {
+  PACKAGE_JSON_BUFFER = fs.readFileSync(path.resolve(PROJECT_DIRECTORY, 'package.json'));
+  PACKAGE_JSON = JSON.parse(PACKAGE_JSON_BUFFER.toString());
+} catch (e) {
+  console.error(`Error: Unable to locate package.json in ${PROJECT_DIRECTORY}`);
+}
+
+const NODE_MODULES_PATH = path.resolve(PROJECT_DIRECTORY, 'node_modules');
+const USE_NODE_MODULES = fs.existsSync(NODE_MODULES_PATH);
 
 if (USE_NODE_MODULES) {
   const allPackageData = getAllPackagesData(NODE_MODULES_PATH);

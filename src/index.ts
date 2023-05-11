1c794bf90311aec785465a947ff8890904c1930b
Tyler <tapoplawski59@gmail.com>
2023-05-11 14:32:22 -0700

    Wrap floating code into separate functions

--- src/index.ts
+++ src/index.ts
@@ -8,21 +8,22 @@ import yargs from 'yargs';
 
 function collectDependencies(
   packageJson: PackageJson,
+  registryPrefix: string,
   includeDevDependencies = true
 ): PackageData[] {
   let dependencies: PackageData[] = [];
   let devDependencies: PackageData[] = [];
 
   if (packageJson?.dependencies) {
-    dependencies = Object.entries(PACKAGE_JSON.dependencies).map(([name, version]) => {
-      const archiveUrl = new URL(`${name}`, REGISTRY_PREFIX).href;
+    dependencies = Object.entries(packageJson.dependencies).map(([name, version]) => {
+      const archiveUrl = new URL(`${name}`, registryPrefix).href;
       return { name, version: version.slice(1), archive: archiveUrl };
     });
   }
 
   if (includeDevDependencies && packageJson?.devDependencies) {
-    devDependencies = Object.entries(PACKAGE_JSON.devDependencies).map(([name, version]) => {
-      const archiveUrl = new URL(`${name}`, REGISTRY_PREFIX).href;
+    devDependencies = Object.entries(packageJson.devDependencies).map(([name, version]) => {
+      const archiveUrl = new URL(`${name}`, registryPrefix).href;
       return { name, version: version.slice(1), archive: archiveUrl };
     });
   }
@@ -30,71 +31,77 @@ function collectDependencies(
   return dependencies.concat(devDependencies);
 }
 
-function parsePackageJson(): PackageJson {
+function parsePackageJson(projectDirectory: string): PackageJson {
   try {
-    const packageJsonBuffer = fs.readFileSync(path.resolve(PROJECT_DIRECTORY, 'package.json'));
+    const packageJsonBuffer = fs.readFileSync(path.resolve(projectDirectory, 'package.json'));
     const packageJson = JSON.parse(packageJsonBuffer.toString());
     return packageJson;
   } catch (e) {
-    console.error(`Error: Unable to locate package.json in ${PROJECT_DIRECTORY}`);
+    console.error(`Error: Unable to locate package.json in ${projectDirectory}`);
     process.exit(1);
   }
 }
 
-const PROJECT_DIRECTORY = '.';
-const REGISTRY_PREFIX = 'https://registry.npmjs.org/';
-const PACKAGE_JSON = parsePackageJson();
-const packages = collectDependencies(PACKAGE_JSON);
-
-// Stop process if selected project directory does not exist or is invalid
-if (!fs.existsSync(PROJECT_DIRECTORY)) {
-  console.error(`Error: ${PROJECT_DIRECTORY} does not exist.`);
-  process.exit(9); // Invalid Argument exit code
-} else if (!isValidPath(PROJECT_DIRECTORY)) {
-  console.error(`Error: ${PROJECT_DIRECTORY} is not a valid path.`);
-  process.exit(9);
+function checkProjectDirectoryExists(projectDirectory: string) {
+  if (!fs.existsSync(projectDirectory)) {
+    console.error(`Error: ${projectDirectory} does not exist.`);
+    process.exit(9); // Invalid Argument exit code
+  } else if (!isValidPath(projectDirectory)) {
+    console.error(`Error: ${projectDirectory} is not a valid path.`);
+    process.exit(9);
+  }
 }
-
-const NODE_MODULES_PATH = path.resolve(PROJECT_DIRECTORY, 'node_modules');
 
 // Main
 
-const processedPackageData = packages.map(async (packageData) => {
-  // Check node_modules first
-  const packagePath = path.join(NODE_MODULES_PATH, packageData.name);
-  packageData.license = LicenseUtils.getLicenseFromNodeModules(packagePath);
+function main() {
+  const PROJECT_DIRECTORY = '.';
+  checkProjectDirectoryExists(PROJECT_DIRECTORY);
 
-  // Check registry if license is not found in node_modules
-  if (!packageData.license) {
-    await LicenseUtils.getLicenseFromRegistry(packageData).then((data) => (packageData = data));
+  const REGISTRY_PREFIX = 'https://registry.npmjs.org/';
+  const PACKAGE_JSON = parsePackageJson(PROJECT_DIRECTORY);
+  const packages = collectDependencies(PACKAGE_JSON, REGISTRY_PREFIX);
+  const NODE_MODULES_PATH = path.resolve(PROJECT_DIRECTORY, 'node_modules');
+
+  const processedPackageData = packages.map(async (packageData) => {
+    // Check node_modules first
+    const packagePath = path.join(NODE_MODULES_PATH, packageData.name);
+    packageData.license = LicenseUtils.getLicenseFromNodeModules(packagePath);
+
+    // Check registry if license is not found in node_modules
+    if (!packageData.license) {
+      await LicenseUtils.getLicenseFromRegistry(packageData).then((data) => (packageData = data));
+    }
+
+    // Populate any missing license descriptions using SPDX repository
+    // https://github.com/spdx/license-list-data/tree/main/text
+    if (packageData.license?.name && !packageData.license?.description) {
+      await LicenseUtils.getLicenseDescription(packageData.license.name).then((desc) => {
+        if (packageData?.license) packageData.license.description = desc;
+      });
+    }
+
+    // Populate any missing package repository URLs
+    if (!packageData?.url) {
+      await LicenseUtils.getRepositoryURL(packageData).then((url) => (packageData.url = url));
+    }
+
+    return packageData;
+  });
+
+  const outputPath = path.join('.', 'output.html');
+  if (!isValidPath(outputPath)) {
+    console.error(`Error: The output ${outputPath} is not a valid path.`);
+    process.exit(9);
   }
 
-  // Populate any missing license descriptions using SPDX repository
-  // https://github.com/spdx/license-list-data/tree/main/text
-  if (packageData.license?.name && !packageData.license?.description) {
-    await LicenseUtils.getLicenseDescription(packageData.license.name).then((desc) => {
-      if (packageData?.license) packageData.license.description = desc;
-    });
-  }
-
-  // Populate any missing package repository URLs
-  if (!packageData?.url) {
-    await LicenseUtils.getRepositoryURL(packageData).then((url) => (packageData.url = url));
-  }
-
-  return packageData;
-});
-
-const outputPath = path.join('.', 'output.html');
-if (!isValidPath(outputPath)) {
-  console.error(`Error: The output ${outputPath} is not a valid path.`);
-  process.exit(9);
+  Promise.all(processedPackageData).then((data) => {
+    saveAsHTML(data, outputPath);
+    console.info('Complete.');
+  });
 }
 
-// Promise.all(processedPackageData).then((data) => {
-//   saveAsHTML(data, outputPath);
-//   console.log('Complete.');
-// });
+main();
 
 yargs
   .scriptName('license-grabber')
